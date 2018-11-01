626ceaebb96b7117574eaeecb635584e0d53878d
Jesse Xu <jessexu.mail@qq.com>
2018-11-01 09:41:59 +0800

    chore: create SwiftyExtensionKit

---
+++ SwiftyExtensionKit/SwiftyExtensionKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyExtensionKit.h
+//  SwiftyExtensionKit
+//
+//  Created by Jesse Xu on 11/1/18.
+//  Copyright © 2018 jessecoding.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyExtensionKit.
+FOUNDATION_EXPORT double SwiftyExtensionKitVersionNumber;
+
+//! Project version string for SwiftyExtensionKit.
+FOUNDATION_EXPORT const unsigned char SwiftyExtensionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyExtensionKit/PublicHeader.h>
+
+
